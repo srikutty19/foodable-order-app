0cee8063d7a928c4d92c0b84ee1487492cb1dcfc
srikutty19 <sri9132@gmail.com>
2019-03-16 11:55:02 +0530

    first commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={			
+	'ApiUrl' : "http://rapidtest.ga/restomulti/merchantapp/api",	
+	'DialogDefaultTitle' : " Fc MERCHANT",
+	'pushNotificationSenderid' : "465251440510",
+	'APIHasKey' : "e7fd6af77a1d45f791880ddebaee29f4"
+};
